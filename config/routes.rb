e402a38152829624406cc75b4f6bc3bb0fa4b20f
Yoann DESCHAUX <yoanndeschaux@gmail.com>
2023-03-16 13:32:42 +0100

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+  get "signup", to: "users#new"
+  get "project", to: "pages#project"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
